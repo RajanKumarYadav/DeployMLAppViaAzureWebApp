048f10efbfb511b89763de06c50541fb433860f4
Rajan <rajan.kumar@live.in>
2024-08-26 21:05:14 +0530

    Azure CD using Github Actions

--- app/app.py
+++ app/app.py
@@ -41,4 +41,4 @@ def diabetesPrediction():
  
             return jsonify(final_predictions) 
 
-app.run(debug=False,host = "0.0.0.0", port = 5000)   
+app.run(debug=False,host = "0.0.0.0", port = 80)   
